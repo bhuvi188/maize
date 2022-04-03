02df052708be1537380bdd1ff03cdac33a0eb999
bhuvi188 <39977830+bhuvi188@users.noreply.github.com>
2022-04-03 15:12:28 -0400

    Update maize_app.py

--- maize_app.py
+++ maize_app.py
@@ -24,8 +24,8 @@ st.markdown("Upload an image of the plant")
 
 images = st.file_uploader("Choose an image...", type=['png', 'jpg' , 'jpeg'])
 submit = st.button('Predict')
-images = Image.open(images)
-images = images.resize((256, 256))
+#images = Image.open(images)
+#images = images.resize((256, 256))
 #images.shape = (1,256,256,3)
 
 #On predict button click
@@ -34,7 +34,7 @@ if submit:
     if images is not None:
         
         
-        
+        st.write(images.type)
         st.write(images.size)
 # show the image
         st.image(images)
