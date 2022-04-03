6c1f1ddd698a1bbf1fbe90e084968c30e229dbed
bhuvi188 <39977830+bhuvi188@users.noreply.github.com>
2022-04-03 15:35:51 -0400

    Update maize_app.py

--- maize_app.py
+++ maize_app.py
@@ -35,6 +35,8 @@ if submit:
         images = images.resize((256, 256))
         st.write(images.mode)
         st.write(images.size)
+        if images.mode is not RGB:
+            st.write("Error")
 # show the image
         st.image(images)
         
