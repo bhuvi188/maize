c09d862d8681dabfe8a56700b02c6fca3d6afe6e
bhuvi188 <39977830+bhuvi188@users.noreply.github.com>
2022-04-03 14:08:13 -0400

    Update maize_app.py

--- maize_app.py
+++ maize_app.py
@@ -23,7 +23,8 @@ st.title("Maize Disease Detection")
 st.markdown("Upload an image of the plant")
 
 images = st.file_uploader("Choose an image...", type=['png', 'jpg' , 'jpeg'])
-images = Image.open(images1)
+images = Image.open(images)
+images = Image.save(images)
 images = images.resize((256, 256))
 #img = image.load_img('geeks.jpg',target_size = (256,256))
 
